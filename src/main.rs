a89ce39fb66ea2250f5a8ea987a92426e92c2429
347Online <38228372+347Online@users.noreply.github.com>
2023-04-30 20:36:33 -0500

    CLI with Clap
    Can now execute a Brainfuck source file when the path is passed as the first argument
    
    Can alse execute a string of Brainfuck passed with -e
    
    Include a hello.bf sample program

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,38 @@
+use std::path::PathBuf;
+
 use brainfuck::Brainfuck;
+use clap::{Parser, ArgGroup};
+
+
+#[derive(Parser, Debug, Default)]
+#[clap(author="Katie Janzen", version, about)]
+#[clap(group(
+    ArgGroup::new("input")
+        .required(false)
+        .args(&["script", "filename"]),
+))]
+/// A Brainfuck Interpreter, written in Rust
+struct Arguments {
+    /// Path to a Brainfuck source file
+    filename: Option<PathBuf>,
+
+    #[clap(short='e')]
+    /// Code to execute
+    script: Option<String>,
+}
 
 fn main() {
+    let args = Arguments::parse();
     let mut bf = Brainfuck::new();
-    let hello_world = ">++++++++[<+++++++++>-]<.>++++[<+++++++>-]<+.+++++++..+++.>>++++++[<+++++++>-]<++.------------.>++++++[<+++++++++>-]<+.<.+++.------.--------.>>>++++[<++++++++>-]<+.";
-    bf.exec(hello_world);
+
+    if let Some(path) = args.filename {
+        match std::fs::read_to_string(path) {
+            Ok(src) => bf.exec(&src),
+            Err(e) => eprintln!("{}", e)
+        }
+    }
+
+    if let Some(src) = args.script {
+        bf.exec(&src);
+    }
 }
